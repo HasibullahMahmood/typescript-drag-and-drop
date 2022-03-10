9cff201c71e15fb5561c31f788f39f2cf41124da
Hasibullah Mahmood <hasibullahmahmood@gmail.com>
2022-03-11 00:42:09 +0300

    get user inputs lesson 125

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
+const AutoBind = (_: any, _2: string, descriptor: PropertyDescriptor) => {
+	const originalMethod = descriptor.value;
+	return {
+		configurable: true,
+		enumerable: false,
+		get() {
+			return originalMethod.bind(this);
+		},
+	};
+};
+
 class ProjectInput {
 	templateEl: HTMLTemplateElement;
 	hostEl: HTMLDivElement;
@@ -20,13 +31,39 @@ class ProjectInput {
 		this.attach();
 	}
 
+	private getInputs(): [string, string, number] | void {
+		const title = this.titleEl.value;
+		const desc = this.descriptionEl.value;
+		const people = this.peopleEl.value;
+
+		if (title.trim().length === 0 || desc.trim().length === 0 || people.trim().length === 0) {
+			alert('Invalid input, please try again!');
+			return;
+		} else {
+			return [title, desc, +people];
+		}
+	}
+
+	private clearInputs() {
+		this.titleEl.value = '';
+		this.descriptionEl.value = '';
+		this.peopleEl.value = '';
+	}
+
+	@AutoBind
 	private submitHandler(event: Event) {
 		event.preventDefault();
-		console.log(this.titleEl.value);
+		const inputs = this.getInputs();
+
+		if (Array.isArray(inputs)) {
+			const [title, desc, people] = inputs;
+			console.log(title, desc, people);
+			this.clearInputs();
+		}
 	}
 
 	private configure() {
-		this.formEl.addEventListener('submit', this.submitHandler.bind(this));
+		this.formEl.addEventListener('submit', this.submitHandler);
 	}
 	private attach() {
 		this.hostEl.insertAdjacentElement('afterbegin', this.formEl);
